3bc6c8f48fc53182b2f09b7bf64ae3795b24de26
Daniel Fiore <taptapdan@gmail.com>
2020-03-06 16:14:23 -0600

    Add set -e to codeship_* scripts

--- codeship_setup.sh
+++ codeship_setup.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+# Commands to be run during a codeship build.
+# In a codeship project, in the Setup Commands window, enter this:
+
+# chmod +x codeship_setup.sh && ./codeship_setup.sh
+
+# This ensures that lines which have an error cause this whole script to
+# exit with an error.
+# http://stackoverflow.com/questions/3474526/stop-on-first-error
+set -e
+
 npm install
--- codeship_test.sh
+++ codeship_test.sh
@@ -1,2 +1,14 @@
-npm lint
+#!/bin/bash
+
+# Commands to be run during a codeship build.
+# In a codeship project, in the Setup Commands window, enter this:
+
+# chmod +x codeship_test.sh && ./codeship_test.sh
+
+# This ensures that lines which have an error cause this whole script to
+# exit with an error.
+# http://stackoverflow.com/questions/3474526/stop-on-first-error
+set -e
+
+npm run lint
 npm test
