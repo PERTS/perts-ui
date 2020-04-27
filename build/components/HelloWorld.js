87ea43d2cfd8c917e1ff5a4fd2b1e5989dac8f87
Daniel Fiore <taptapdan@gmail.com>
2020-04-27 15:49:03 -0700

    Include build directory in repo

---
+++ build/components/HelloWorld.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var HelloWorld = function HelloWorld(_ref) {
+  var name = _ref.name;
+  return /*#__PURE__*/_react["default"].createElement("div", null, "Hello ", name, "!");
+};
+
+HelloWorld.propTypes = {
+  name: _propTypes["default"].string
+};
+HelloWorld.defaultProps = {
+  name: 'World'
+};
+var _default = HelloWorld;
+exports["default"] = _default;
---
+++ build/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "HelloWorld", {
+  enumerable: true,
+  get: function get() {
+    return _HelloWorld["default"];
+  }
+});
+
+var _HelloWorld = _interopRequireDefault(require("./components/HelloWorld"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
