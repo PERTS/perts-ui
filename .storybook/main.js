8c284baac53edb3f64a4ea66ca6d5c0b2c88c1d5
Daniel Fiore <taptapdan@gmail.com>
2020-04-28 11:25:39 -0700

    Add storybook repo links to main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,11 +4,17 @@ module.exports = {
     '../lib/**/*.stories.js',
   ],
   addons: [
+    // https://github.com/storybookjs/storybook/tree/master/addons/knobs
     '@storybook/addon-knobs/register',
+    // https://github.com/storybookjs/storybook/tree/master/addons/actions
     '@storybook/addon-actions',
+    // https://github.com/storybookjs/storybook/tree/master/addons/links
     '@storybook/addon-links',
+    // https://github.com/storybookjs/storybook/tree/master/addons/storysource
     '@storybook/addon-storysource',
+    // https://github.com/storybookjs/storybook/tree/master/addons/a11y
     '@storybook/addon-a11y/register',
+    // https://github.com/storybookjs/storybook/tree/master/addons/viewport
     '@storybook/addon-viewport/register',
   ],
 };
