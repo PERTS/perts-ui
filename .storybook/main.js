d55a7800e1d9fa9f3805a4c1aa1cef1c2edb28df
Daniel Fiore <taptapdan@gmail.com>
2020-04-28 10:38:41 -0700

    Add storybook storysource

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,11 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js', '../lib/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  stories: [
+    '../stories/**/*.stories.js',
+    '../lib/**/*.stories.js',
+  ],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-storysource',
+  ],
 };
