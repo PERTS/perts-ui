c20d81e87e5d89f1e6e1ec8db34f48ffda2c1bfe
Daniel Fiore <taptapdan@gmail.com>
2020-04-30 09:52:53 -0700

    Remove StylesAlignVertically component (replaced by build/styles)

--- build/index.js
+++ build/index.js
@@ -3,20 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "StylesAlignVertically", {
-  enumerable: true,
-  get: function get() {
-    return _GlobalStyles.StylesAlignVertically;
-  }
-});
 Object.defineProperty(exports, "HelloWorld", {
   enumerable: true,
   get: function get() {
     return _HelloWorld["default"];
   }
 });
-
-var _GlobalStyles = require("./components/GlobalStyles");
 
 var _HelloWorld = _interopRequireDefault(require("./components/HelloWorld"));
 
