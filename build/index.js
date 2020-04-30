21cec5395324cb041605c927898352af4ea3b291
Daniel Fiore <taptapdan@gmail.com>
2020-04-30 15:45:22 -0700

    Update build

--- build/index.js
+++ build/index.js
@@ -9,7 +9,23 @@ Object.defineProperty(exports, "HelloWorld", {
     return _HelloWorld["default"];
   }
 });
+Object.defineProperty(exports, "ListItem", {
+  enumerable: true,
+  get: function get() {
+    return _ListItem["default"];
+  }
+});
+Object.defineProperty(exports, "UnorderedList", {
+  enumerable: true,
+  get: function get() {
+    return _UnorderedList["default"];
+  }
+});
 
 var _HelloWorld = _interopRequireDefault(require("./components/HelloWorld"));
+
+var _ListItem = _interopRequireDefault(require("./components/ListItem"));
+
+var _UnorderedList = _interopRequireDefault(require("./components/UnorderedList"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
